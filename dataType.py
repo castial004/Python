b414431f4b009ea74d92b6f459ff703b7ee19203
castial004 <nm6395825@gmail.com>
2026-06-03 23:35:48 +0530

    initial commit

---
+++ dataType.py
@@ -0,0 +1,17 @@
+# int
+val1 = 20 
+print("Value of data type 1: ",val1, "and its type is: ", type(val1))
+# float
+val2 = 20.20
+print("Value of data type 1: ",val2, "and its type is: ", type(val2))
+# boolean
+val3 = False
+print("Value of data type 3: ",val3, "and its type is: ", type(val3))
+val4 = True
+print("Value of data type 4: ",val4, "and its type is: ", type(val4))
+# string
+val5 = "Hello"
+print("Value of data type 5: ",val5, "and its type is: ", type(val5))
+# None
+val6 = None
+print("Value of data type 6: ",val6, "and its type is: ", type(val6))
---
+++ inputAndoutput.py
@@ -0,0 +1,6 @@
+# taking input
+name = input("enter your name: ")
+# age
+age = int(input("enter your age: "))
+
+print("Hello!!! ",name," ",age)
