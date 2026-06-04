7c48b171e98792073e00ac84b8b0fb9bc814ffdc
castial004 <nm6395825@gmail.com>
2026-06-04 23:51:27 +0530

    implicit and explicit conversion

---
+++ Explicit_Conversion.py
@@ -0,0 +1,10 @@
+val1 = 20
+val2 = 30.05
+#This is implicit conversion
+val3 = val1+val2 
+print(val3)
+print(type(val3))
+#This is explicit conversion
+val4 = int(val1+val2)
+print(val4)
+print(type(val4))
